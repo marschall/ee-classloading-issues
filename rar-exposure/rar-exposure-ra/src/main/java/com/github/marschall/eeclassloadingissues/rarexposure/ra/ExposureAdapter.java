c0e7042f4d5eb35be08859f992954a536a39adc3
Philippe Marschall <philippe.marschall@gmail.com>
2013-11-11 09:07:34 +0100

    add JNDI binding

--- rar-exposure/rar-exposure-ra/src/main/java/com/github/marschall/eeclassloadingissues/rarexposure/ra/ExposureAdapter.java
+++ rar-exposure/rar-exposure-ra/src/main/java/com/github/marschall/eeclassloadingissues/rarexposure/ra/ExposureAdapter.java
@@ -1,21 +1,39 @@
 package com.github.marschall.eeclassloadingissues.rarexposure.ra;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.resource.spi.ActivationSpec;
 import javax.resource.spi.BootstrapContext;
 import javax.resource.spi.Connector;
 import javax.resource.spi.ResourceAdapter;
+import javax.resource.spi.ResourceAdapterInternalException;
 import javax.resource.spi.endpoint.MessageEndpointFactory;
 import javax.transaction.xa.XAResource;
 
 @Connector
 public class ExposureAdapter implements ResourceAdapter {
 
+  private static final String NAME = "java:global/some/name";
+  private InitialContext context;
+
   @Override
-  public void start(BootstrapContext ctx) {
+  public void start(BootstrapContext ctx) throws ResourceAdapterInternalException {
+    try {
+      context = new InitialContext();
+      context.bind(NAME, "aString");
+    } catch (NamingException e) {
+      throw new ResourceAdapterInternalException("could not bind to: " + NAME, e);
+    }
   }
 
   @Override
   public void stop() {
+    try {
+      this.context.unbind(NAME);
+      this.context.close();
+    } catch (NamingException e) {
+      throw new RuntimeException("could not unbind:" + NAME, e);
+    }
   }
 
   @Override
